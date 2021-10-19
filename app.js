ab9031f419257c415b4e74b8a7916d8a957e8095
María Camila Niebles Reyes <cnieblesr@MacBook-Pro-de-Maria.local>
2021-10-18 23:03:12 -0500

    Initial configuration to mongodb

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
+const getConnection = require('./src/config/mongo.config')
 const port = process.env.PORT
 
 const app = express()
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+
+try {
+  getConnection({
+    user: process.env.MONGO_USER,
+    password: process.env.MONGO_PASSWORD,
+    cluster: process.env.MONGO_CLUSTER,
+    database: process.env.MONGO_DATABASE
+  })
+} catch (error) {
+  console.log(error)
+}
 
 app.listen(port, () => {
   console.log(`App listen in port ${port}`)
