97e673af37715c08b4a981c15258d42267098218
Stiben <jstibenpb@gmail.com>
2021-11-27 15:58:34 -0500

    company crud

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ const bodyParser = require('body-parser')
 const getConnection = require('./src/config/database.init')
 const port = process.env.PORT
 const proxy = require('./src/routes/proxy')
+const company = require('./src/routes/company');
 
 const app = express()
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use('/rchilli', proxy)
+app.use('/company', company);
 
 try {
   getConnection(process.env.MONGO_REPLICA_SET)
--- src/repositories/company.repository.js
+++ src/repositories/company.repository.js
@@ -1,4 +1,107 @@
 const RchilliCompany = require('../models/company.model')
+const Company = require('../models/company2.model')
+
+const { ObjectId } = require('mongodb');
+
+/*
+ * It creates a company in DB
+ */
+const createCompany = async (data) => {
+  try {
+    data.active = true;
+
+    return await new Company(data).save();
+  } catch (error) {
+    throw error;
+  }
+};
+
+/*
+ * It looks for a company according to the name
+ */
+const getCompanyByName = async (name) => {
+  try {
+    return await Company.findOne({ name, active: true });
+  } catch (error) {
+    throw error;
+  }
+};
+
+/*
+ * It looks for a company according to the id
+ */
+const getCompanyById = async (id) => {
+  try {
+    return await Company.findOne({ '_id': new ObjectId(id), active: true });
+  } catch (error) {
+    throw error;
+  }
+};
+
+/*
+ * It looks for a company according to the domain
+ */
+const getCompanyByDomain = async (domain) => {
+  try {
+    return await Company.findOne({ domain, active: true });
+  } catch (error) {
+    throw error;
+  }
+};
+
+/*
+ * It looks for a company according to the domain or name
+ */
+const getCompanyByDomainOrName = async (name, domain) => {
+  try {
+    return await Company.findOne({
+      $or: [
+             { name },
+             { domain }
+           ],
+      $and: [{ active: true }],
+    });
+  } catch (error) {
+    throw error;
+  }
+};
+
+/*
+ * It updates the data of a company
+ */
+const updateCompanyById = async (id, data) => {
+  try {
+    return await Company.updateOne(
+      { _id: id },
+      {
+        $set: {
+          name: data.name,
+          domain: data.domain,
+        }
+      }
+    );
+  } catch (error) {
+    throw error;
+  }
+};
+
+/*
+ * It deletes a company
+ */
+const deleteCompanyById = async (id, data) => {
+  try {
+    return await Company.updateOne(
+      { _id: id },
+      {
+        $set: {
+          active: false,
+        }
+      }
+    );
+  } catch (error) {
+    throw error;
+  }
+};
 
 const createRecord = async function (rchilliData) {
   try {
@@ -21,5 +124,12 @@ const getByEmail = async function (email) {
 
 module.exports = {
   createRecord,
-  getByEmail
-}
+  getByEmail,
+  createCompany,
+  getCompanyByName,
+  getCompanyById,
+  getCompanyByDomain,
+  getCompanyByDomainOrName,
+  updateCompanyById,
+  deleteCompanyById,
+};
